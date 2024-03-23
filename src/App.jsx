9935a087e2ec3997112a3b499e58c66b84cbdea2
tulsirampathe <tulsirampathe81@gmail.com>
2024-03-23 14:14:32 +0530

    change background

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Update from "./components/Update";
 function App() {
   return (
     <div>
-      <div className="mainContainer"></div>
+      <div className="mainContainer bg-gradient-to-l from-gray-500 to-purple-300"></div>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home1 />} />
